db83cc73ef16419556a2d790e43dd3c91fded73c
hyungjunk <khjgd2@gmail.com>
2020-09-23 23:42:31 +0900

    WIP

--- my_unsplash/src/App.js
+++ my_unsplash/src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import './App.css';
 import './assets/main.css';
+import { faCamera, faSearch } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 function App() {
+  // use fontawesome
+  // https://mdbootstrap.com/docs/react/content/icons-list/
+  // const camera = <FontAwesomeIcon icon={faCamera} />
+
+  const searchBtn = <FontAwesomeIcon icon={faSearch} />
+
   return (
     <div className="App">
-      <div className="m-5">
-        <span className="hash-tag">#HashTag</span>
+      <div className="text-center mx-10 mt-10 mb-10 h-20 bg-logo">
+        <FontAwesomeIcon style={{ marginRight: '30px' }} icon={faSearch} />
+        <input className="border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 align-middle" placeholder="Search your images..." />
+        <button className="bg-blue-500 float-right hover:bg-gray-600 text-white hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ml-10">Add Photo</button>
       </div>
-      <div className="card-wrapper">
-        <h5 className="card-title">My Title</h5>
-        <p className="text-gray-700 text-lg">Content goes here</p>
+      <div class="flex mb-4">
+        <div className="bg-sample card-wrapper rounded-lg m-5 w-1/4 h-auto" />
+        <div className="card-wrapper rounded-lg m-5 w-1/4" />
+        <div className="card-wrapper rounded-lg m-5 w-1/4" />
+        <div className="card-wrapper rounded-lg m-5 w-1/4">
+          <h5 className="card-title">My Title</h5>
+          <p className="text-gray-700 text-lg">Content goes here</p>
+        </div>
       </div>
     </div>
   );
--- my_unsplash/src/todo.md
+++ my_unsplash/src/todo.md
@@ -1,9 +1,7 @@
-9/16
+9/25
 
-1. 카드가 들어갈 레이아웃 박스 만들기
-2. 카드 Item 만들기
-3. 로고 만들기
-4. 인풋 박스 만들기
-5. 서치 버튼 만들기
+- 버튼 액션 구현
+- Modal 띄우기
+- 지난번에 사용했던 image uploader 써보기
 
 [링크](https://devchallenges.io/challenges/rYyhwJAxMfES5jNQ9YsP)
