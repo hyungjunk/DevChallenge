ab08142debe32957497303f2296b33fb79729cfb
hyungjunk <khjgd2@gmail.com>
2020-11-17 00:36:10 +0900

    은근 복잡한듯 이것도 후에 refactoring이 필요할 듯 하다.

--- country_quiz/src/AnswerChoice.js
+++ country_quiz/src/AnswerChoice.js
@@ -1,7 +1,8 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useContext } from "react";
 import styled from "styled-components";
 import CorrectSVGIcon from "./assets/correct.svg";
 import WrongSVGIcon from "./assets/wrong.svg";
+import { AnswerContext } from "./QuizCard";
 
 const Styled = styled.button`
   position: relative;
@@ -29,13 +30,20 @@ const StyledAnswerMark = styled.span`
 `;
 
 const ICONWrapper = styled.div`
-  display: none;
+  display: ${(props) =>
+    props.isVisible && (props.userSelected || props.isAnswer)
+      ? "block"
+      : "none"};
 `;
 
-const AnswerICON = ({ isAnswer }) => {
+const AnswerICON = ({ userSelected, isVisible, isAnswer }) => {
   const iconSRC = isAnswer ? CorrectSVGIcon : WrongSVGIcon;
   return (
-    <ICONWrapper>
+    <ICONWrapper
+      userSelected={userSelected}
+      isVisible={isVisible}
+      isAnswer={isAnswer}
+    >
       <img src={iconSRC} />
     </ICONWrapper>
   );
@@ -45,18 +53,31 @@ const AnswerLetter = ({ letter }) => {
   return <StyledLetter>{letter}</StyledLetter>;
 };
 
+const handleClick = (setContext, choice, isAnswer) => {
+  setContext({
+    userHasTried: true,
+    userSelected: choice,
+    isUserCorrect: isAnswer,
+  });
+};
+
 const AnswerChoice = forwardRef((props, ref) => {
-  const { letter, choice, answer, handleClick } = props;
+  const [context, setContext] = useContext(AnswerContext);
+  const { letter, choice, isAnswer } = props;
   return (
     <Styled
       isLongText={choice.length > 30}
-      onClick={(e) => handleClick(e, choice, answer)}
+      onClick={() => handleClick(setContext, choice, isAnswer)}
       ref={ref}
     >
       <AnswerLetter letter={letter} />
       <span>{choice}</span>
       <StyledAnswerMark>
-        <AnswerICON isAnswer={choice === answer} />
+        <AnswerICON
+          userSelected={context.userSelected === choice}
+          isVisible={context.userHasTried}
+          isAnswer={isAnswer}
+        />
       </StyledAnswerMark>
     </Styled>
   );
--- country_quiz/src/QuizCard.js
+++ country_quiz/src/QuizCard.js
@@ -1,4 +1,10 @@
-import React, { useRef } from "react";
+import React, {
+  createContext,
+  useContext,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import styled from "styled-components";
 import QuizQuestion from "./QuizQuestion";
 import AnswerChoice from "./AnswerChoice";
@@ -22,44 +28,50 @@ const AnswerContainer = styled.div`
   padding: 50px 0px;
 `;
 
+const INITIAL_STATE = {
+  userSelected: null,
+  answer: null,
+  userHasTried: false,
+  isUserCorrect: false
+};
+export const AnswerContext = createContext();
+
 const QuizCard = () => {
+  const [context, setContext] = useState(INITIAL_STATE);
   const isAnswerRef = useRef();
-  const sampleRef = useRef();
   const { sentence, choices, answer } = useQuestions();
   const CHAR_CODE_A = 65;
   const handleClick = (e) => {
     // e.target.style.background = "red";
     // isAnswerRef.current.style.background = "green";
-
     console.log(isAnswerRef.current);
     console.log(isAnswerRef.current.children);
-    console.log(isAnswerRef.current.children[2].children[0].style.display='block');
+    console.log(
+      (isAnswerRef.current.children[2].children[0].style.display = "block")
+    );
   };
 
-  // Answer container를 랩핑하는 context provider를 두기
-  // answer, userSelect를 initialState로 두기
-  // handleClick=> setUserSelect(true)로 만듬
   // userSelect에 따라 AnswerChoice 컴포넌트에서는 background color, icon이보이게/안보이게
   // 이외 많은 것들이 필요없어질수도.
 
   // FIXME: 굳이 answer 필요없고 isAnswer={choice====answer}로 해버려도 될 듯
-
   return (
     <Styled>
       <QuizQuestion sentence={sentence} />
-      <AnswerContainer>
-        {!choices && <h1>Loading...</h1>}
-        {choices?.map((choice, idx) => (
-          <AnswerChoice
-            ref={choice === answer ? isAnswerRef : null}
-            key={choice}
-            letter={String.fromCharCode(idx + CHAR_CODE_A)}
-            choice={choice}
-            answer={answer}
-            handleClick={handleClick}
-          />
-        ))}
-      </AnswerContainer>
+      <AnswerContext.Provider value={[context, setContext]}>
+        <AnswerContainer>
+          {!choices && <h1>Loading...</h1>}
+          {choices?.map((choice, idx) => (
+            <AnswerChoice
+              ref={choice === answer ? isAnswerRef : null}
+              key={choice}
+              letter={String.fromCharCode(idx + CHAR_CODE_A)}
+              choice={choice}
+              isAnswer={choice === answer}
+            />
+          ))}
+        </AnswerContainer>
+      </AnswerContext.Provider>
     </Styled>
   );
 };
