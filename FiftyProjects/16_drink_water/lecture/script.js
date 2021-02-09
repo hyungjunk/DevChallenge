9af0f9a927a53f2d40e95d08880add18e1c023a1
hyungjunk <khjgd2@gmail.com>
2021-02-09 19:00:59 +0900

    16 lecture done

--- FiftyProjects/16_drink_water/lecture/script.js
+++ FiftyProjects/16_drink_water/lecture/script.js
@@ -1,14 +1,50 @@
-const cups = document.querySelectorAll(".cup-small");
+const smallCups = document.querySelectorAll(".cup-small");
+const percentage = document.querySelector("#percentage");
+const liters = document.querySelector("#liters");
+const remained = document.querySelector("#remained");
+const MEASURE = 0.25;
 
 const highlightCups = (cupIdx) => {
-  cups.forEach((cup, idx) => {
-    console.log(cup);
+  if (
+    smallCups[cupIdx].classList.contains("full") &&
+    !smallCups[cupIdx].nextElementSibling?.classList.contains("full")
+  ) {
+    cupIdx--;
+  }
+  smallCups.forEach((cup, idx) => {
     if (idx <= cupIdx) {
       cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
     }
   });
+  updateBigCup();
 };
 
-cups.forEach((cup, idx) => {
-  cup.addEventListener("click", () => highlightCups(idx));
+const updateBigCup = () => {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    liters.innerText = 2 - MEASURE * fullCups + "L";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = ((2 * fullCups) / 16) * 100 + "%";
+  }
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = 2 - MEASURE * fullCups + "L";
+  }
+};
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => {
+    highlightCups(idx);
+  });
 });
