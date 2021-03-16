dc48bff9712a9fbe3f6c7e00b7b8ee2590ca1d53
hyungjunk <khjgd2@gmail.com>
2021-03-16 14:54:32 +0900

    update ripple btn

---
+++ FiftyProjects/20_ripple_button/lecture/script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll(".ripple");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    const { x, y } = e;
+    console.log({ x, y });
+    const buttonTop = e.target.offsetTop;
+    const buttonLeft = e.target.offsetLeft;
+    console.log({ buttonTop, buttonLeft });
+
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+    console.log({ xInside, yInside });
+
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    circle.style.top = `${yInside}px`;
+    circle.style.left = `${xInside}px`;
+
+    console.log(this);
+    this.appendChild(circle);
+    setTimeout(() => circle.remove(), 500);
+  });
+});
